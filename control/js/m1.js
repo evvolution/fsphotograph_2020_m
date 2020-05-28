bfd6f372a16f0575a62888d5d822a7e77559f24a
star_dust_evolution <429736040@qq.com>
2020-05-28 15:03:04 +0800

    final version

--- control/js/m1.js
+++ control/js/m1.js
@@ -1,6 +1,6 @@
 
 document.ready(function(){
-    isInfsplus()
+    // isInfsplus()
 });
 
 function closenotice() {
@@ -40,7 +40,29 @@ function allsumbit() {
     var identify_id = document.getElementById("prjidentify").value.length;
     var content_500 = document.getElementById("shortprjmain").value.length;
     var content = document.getElementById("shortprjverify").value.length;
-    var fimg = document.getElementById("prjcover").value.length;
+
+    var fimg = document.getElementById("prjcover"); //获取整体file
+    var isFile = fimg.value.length; //文件是否存在
+
+    // console.log(typeFile);
+    // if (isFile === 0) {
+    //     alert("请上传作品");
+    //     return;
+    // }
+    // if (!((typeFile === "jpg") || (typeFile === "JPG") || (typeFile === "JPEG") || (typeFile === "jpeg") || (typeFile === "raw") || (typeFile === "RAW"))) {
+    //     alert("请上传jpg，jpeg或raw类型的作品");
+    //     return;
+    // }
+    // if (countFile != 1) {
+    //     alert("只可以上传一个作品文件");
+    //     return;
+    // }
+    // if ((sizeFile < 2048) || (sizeFile > 20480)){
+    //     alert("文件大小需在2M-20M");
+    //     return;
+    // }
+
+    // return;
 
     if (title === 0) {
         alert("作品名称不可为空");
@@ -70,10 +92,31 @@ function allsumbit() {
         alert("请填写作品简介");
         return;
     }
-    if (fimg === 0) {
+
+    /*-----------*/
+    if (isFile === 0) {
         alert("请上传作品");
         return;
     }
+
+    var typeFile = fimg.files[0].type.split("/")[1]; //文件类型JPG,JPEG,RAW
+    var countFile = fimg.files.length; //文件数量只能为1
+    var sizeFile = fimg.files[0].size / 1024; //文件大小
+
+    if (!((typeFile === "jpg") || (typeFile === "JPG") || (typeFile === "JPEG") || (typeFile === "jpeg") || (typeFile === "raw") || (typeFile === "RAW"))) {
+        alert("请上传jpg，jpeg或raw类型的作品");
+        return;
+    }
+    if (countFile != 1) {
+        alert("只可以上传一个作品文件");
+        return;
+    }
+    if ((sizeFile < 2048) || (sizeFile > 20480)){
+        alert("文件大小需在2M-20M");
+        return;
+    }
+
+
     ajax_postinfo(formall)
 }
 
@@ -93,7 +136,7 @@ function ajax_postinfo(data) {
             var strid = success_id.toString();
             // console.log(strid);
             var strid_md5 = strid.MD5().toLocaleUpperCase();
-            $("#successcode").html(strid_md5);
+            $("#successcode").val(strid_md5);
             $("#code").css("display","block")
         },
         error: function(receiver){
@@ -102,4 +145,12 @@ function ajax_postinfo(data) {
             return;
         }
     });
+}
+
+function clipToborad() {
+    var target = document.getElementById("successcode");
+    // console.log(target)
+    target.select();
+    document.execCommand("Copy");
+    alert("已复制到剪切板，可粘贴。");
 }
